cec9222b61f37a7ba8df04193c0e0ac7b5843624
yihsunphys <138319302+yihsunphys@users.noreply.github.com>
2024-06-28 21:13:48 +0800

    Delete code directory

--- code/zerojudge/a065.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-   string s;
-   cin>>s;
-   for(int i=0;i<6;i++){
-     cout<<abs(s[i+1]-s[i]);
-   }
-}
--- code/zerojudge/a095.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-   int a,b;
-   while(cin>>a>>b){
-      if(a!=b) cout<<b+1<<endl;
-      if(a==b) cout<<b<<endl;
-   }
-}
