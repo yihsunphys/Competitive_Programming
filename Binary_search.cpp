613d2da93a375a756e595d7e5cdafa20aa4ebafc
yihsun <yqiu789@gmail.com>
2024-07-02 02:41:50 +0800

    20240702 update

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -1,16 +1,35 @@
-int upper_bound(int *a, int begin, int end, int key) {
-  int first = begin, last = end-1;
-  int middle, pos = begin;       
-  while(first < last){
-    middle = (first+last)/2;
-    if(a[middle] <= key){ 
-        first = middle + 1;  
-        pos = first;   
-    }
-    else{
-        last = middle;   
-        pos = last;                    
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+int my_upper_bound(int *a, int l, int r, int key) { //大於key的最小值
+  while(l <= r) {
+    int mid = (l+r)>>1;
+    if(a[mid]>key) r = mid-1;
+    else l = mid+1;
   }
-  return pos;
+  return l;
+}
+
+int my_lower_bound(int *a, int l, int r, int key) { //大於等於key的最小值
+  while(l <= r) {
+    int mid = (l+r)>>1;
+    if(a[mid]>=key) r = mid-1;
+    else l = mid+1;
+  }
+  return l;
+}
+
+int my_binary_search(int *a, int l, int r, int key) { //大於等於key的最小值
+  while(l <= r) {
+    int mid = (l+r)>>1;
+    if(a[mid]>key) r = mid-1;
+    else if(a[mid]<key)l = mid+1;
+    else return mid;
+  }
+  return -1;
+}
+
+int main() {
+  int a[5] = {1, 2, 3, 4, 5};
+  cout << my_lower_bound(a, 0, 5, 3);
 }
